5d6cd0482152f6ee1174ed8b6a8379693e1e64f1
liviuq <petrache.andrei1@gmail.com>
2022-04-13 15:42:18 +0300

    Started work on the landing page

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Video Review</title>
+    <link rel="stylesheet" href="sass/index.css">
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <header>
+        <img class="logo" src="img/test_icon.png" alt="logo">
+        <div class="button-box">
+             <div class="dropdown" data-dropdown>
+                <button type="button" class="toggle-button" data-dropdown-button>Categories</button>
+                <div class="dropdown-menu">
+                    Dropdown content 
+                </div>
+             </div>
+            <button type="button" class="toggle-button" >About Us</button>
+            <button type="button" class="toggle-button" >Contact</button>
+        </div>
+        <div class="wrapper">
+            <img class="search-icon" src="img/search_icon.png" alt="search"/>
+            <input class="search" placeholder="Search" type="text" >
+        </div> 
+    </header>
+<!--    Start of the title-->
+    <div class="row">
+        <h2> Video Reviews made easy!</h2>
+        <div class="row__posters">
+            <img src="https://picsum.photos/1280/720" alt="a simple image" class="row__poster row__posterLarge">
+            <img src="https://picsum.photos/1280/720" alt="a simple image" class="row__poster row__posterLarge">
+            <img src="https://picsum.photos/1280/720" alt="a simple image" class="row__poster row__posterLarge">
+            <img src="https://picsum.photos/1280/720" alt="a simple image" class="row__poster row__posterLarge">
+            <img src="https://picsum.photos/1280/720" alt="a simple image" class="row__poster row__posterLarge">
+            <img src="https://picsum.photos/1280/720" alt="a simple image" class="row__poster row__posterLarge">
+            <img src="https://picsum.photos/1280/720" alt="a simple image" class="row__poster row__posterLarge">
+            <img src="https://picsum.photos/1280/720" alt="a simple image" class="row__poster row__posterLarge">
+
+        </div>
+    </div>
+<!--    Trending now-->
+    <div class="row">
+        <h2>Trending now</h2>
+        <div class="row__posters">
+            <img src="https://picsum.photos/121/220" alt="a simple image" class="row__poster">
+            <img src="https://picsum.photos/121/220" alt="a simple image" class="row__poster">
+            <img src="https://picsum.photos/121/220" alt="a simple image" class="row__poster">
+            <img src="https://picsum.photos/121/220" alt="a simple image" class="row__poster">
+            <img src="https://picsum.photos/121/220" alt="a simple image" class="row__poster">
+            <img src="https://picsum.photos/121/220" alt="a simple image" class="row__poster">
+            <img src="https://picsum.photos/121/220" alt="a simple image" class="row__poster">
+            <img src="https://picsum.photos/121/220" alt="a simple image" class="row__poster">
+
+        </div>
+    </div>
+<!--    Top Rated-->
+    <div class="row">
+        <h2>Top Rated</h2>
+        <div class="row__posters">
+            <img src="https://picsum.photos/121/220" alt="a simple image" class="row__poster">
+            <img src="https://picsum.photos/121/220" alt="a simple image" class="row__poster">
+            <img src="https://picsum.photos/121/220" alt="a simple image" class="row__poster">
+            <img src="https://picsum.photos/121/220" alt="a simple image" class="row__poster">
+            <img src="https://picsum.photos/121/220" alt="a simple image" class="row__poster">
+            <img src="https://picsum.photos/121/220" alt="a simple image" class="row__poster">
+            <img src="https://picsum.photos/121/220" alt="a simple image" class="row__poster">
+            <img src="https://picsum.photos/121/220" alt="a simple image" class="row__poster">
+
+        </div>
+    </div>
+</body>
+</html>
