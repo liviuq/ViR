4b9e9fa5da47096a10392daa29aa7b8668411d94
liviuq <petrache.andrei1@gmail.com>
2022-05-30 19:09:21 +0300

    Cleaned the code

--- js/script.js
+++ js/script.js
@@ -1,29 +1,10 @@
-document.addEventListener('click',e=>{
-    const isDropdownButton = e.target.matches("[data-dropdown-button]")
-    if(!isDropdownButton&&e.target.closest("[data-dropdown]")!=null)
-    return
-
-    let currentDropdown
-    if(isDropdownButton){
-    currentDropdown=e.target.closest("[data-dropdown]")
-    currentDropdown.classList.toggle("active")
-    }
-
-    document.querySelectorAll("[data-dropdown].active").forEach(dropdown=>{
-        if(dropdown===currentDropdown) return
-        dropdown.classList.remove("active")
-    })
-});
-
-const nav = document.getElementById('nav');
-
 window.addEventListener('scroll', () => {
     if(window.scrollY > 100)
     {
-        nav.classList.add('nav__black');
+        document.getElementById('nav').classList.add('nav__black');
     }
     else
     {
-        nav.classList.remove('nav__black');
+        document.getElementById('nav').classList.remove('nav__black');
     }
 });
