b80f1059757284e07e699db15979875650b04b66
liviuq <petrache.andrei1@gmail.com>
2022-05-30 17:37:06 +0300

    Implemented card fetching and displaying

--- js/cards.js
+++ js/cards.js
@@ -5,6 +5,13 @@ function getCards()
     .then((res) => res.json())
     .then((data) => {
         //here you start to edit the DOM
+        let cards = '';
+        data['data'].forEach(element => {
+            //console.log(element);
+            cards += `<img src=${element['banner']} class="row__poster">`;
+        });
+
+        document.getElementById('firstrow').innerHTML = cards;
     });
 }
 
