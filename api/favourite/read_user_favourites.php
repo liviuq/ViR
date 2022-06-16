ac52a4737631477c02b671b698741566815cd5a3
liviuq <petrache.andrei1@gmail.com>
2022-06-16 17:01:38 +0300

    Update read_user_favourites.php

--- api/favourite/read_user_favourites.php
+++ api/favourite/read_user_favourites.php
@@ -2,7 +2,7 @@
     //headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
-    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Methods: GET');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With'); //helps with auth, cross site scripting, cors
 
     include_once '../../config/Database.php';
