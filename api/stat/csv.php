d7fcce6c39a574d1ac4ab22894bfc7718ec7a1ac
MarcuEduardA3 <marcu.eduard37@gmail.com>
2022-06-18 20:56:17 +0300

    debugging

--- api/stat/csv.php
+++ api/stat/csv.php
@@ -48,7 +48,8 @@
                 'aired' => $aired,
                 'genre' => $genre
             );
-            if($aired=='1')
+            
+            if($aired=="1")
             $aired_temp =  'Completed';
             else
             $aired_temp =  'In progress';
