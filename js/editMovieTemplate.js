680b836da2acd54f0ea84174aa0f991bd82f9af8
liviuq <petrache.andrei1@gmail.com>
2022-06-19 13:43:05 +0300

    Refactored functions to editMovieTemplate.js

--- js/editMovieTemplate.js
+++ js/editMovieTemplate.js
@@ -97,3 +97,26 @@ async function submitReview(e){
     console.log(reply);
     //marcu will do this
 }
+
+document.getElementById('addfav__button')
+.addEventListener('click', addToFav);
+
+async function addToFav()
+{
+    //check to see if we have the token
+    const cookieValue = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('token='))
+        ?.split('=')[1];
+    const res = await fetch(`https://vira3.herokuapp.com/api/favourite/create.php?id=${id}`, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-type':'application/json',
+            'Authorization': `Bearer ${cookieValue}`
+        }
+        });
+        const reply = await res.json();
+        console.log(reply);
+    
+}
