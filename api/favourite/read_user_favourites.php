ceb24c3b386c4097a26dac6698003c84ca01ce4a
liviuq <petrache.andrei1@gmail.com>
2022-06-19 17:36:20 +0300

    Modified xml dump to display xml properly

--- api/favourite/read_user_favourites.php
+++ api/favourite/read_user_favourites.php
@@ -3,6 +3,7 @@
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: GET');
+    header('Content-Type: text/xml');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With'); //helps with auth, cross site scripting, cors
 
     include_once '../../config/Database.php';
