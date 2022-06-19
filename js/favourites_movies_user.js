3dcfa3a9cf8d160cd72fe17ad71f3643f4a46167
MarcuEduardA3 <marcu.eduard37@gmail.com>
2022-06-19 16:12:12 +0300

    Update favourites_movies_user.js

--- js/favourites_movies_user.js
+++ js/favourites_movies_user.js
@@ -1,19 +1,22 @@
-const cookieValue = document.cookie
-        .split('; ')
-        .find(row => row.startsWith('token='))
-        ?.split('=')[1];
+async function getFavJson(){
+    const cookieValue = document.cookie
+    .split('; ')
+    .find(row => row.startsWith('token='))
+    ?.split('=')[1];
 
 
-    const res = await fetch(`https://vira3.herokuapp.com/api/favourite/read_user_favourites.php`, {
-        method: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-type':'application/json',
-            'Authorization': `Bearer ${cookieValue}`
-        }
-        });
-        const reply = await res.json();
-        console.log(reply);
+const res = await fetch(`https://vira3.herokuapp.com/api/favourite/read_user_favourites.php`, {
+    method: "POST",
+    headers: {
+        'Accept': 'application/json',
+        'Content-type':'application/json',
+        'Authorization': `Bearer ${cookieValue}`
+    }
+    });
+    const reply = await res.json();
+    console.log(reply);
 
-        var str= JSON.stringify(reply);
-        document.body.appendChild(document.createElement("pre")).innerHTML=str;
+    var str= JSON.stringify(reply);
+    document.body.appendChild(document.createElement("pre")).innerHTML=str;
+}
+getFavJson();
