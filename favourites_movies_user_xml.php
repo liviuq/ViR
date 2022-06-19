b7966457ab99f9f089040a08f2698e27485cfcb7
MarcuEduardA3 <marcu.eduard37@gmail.com>
2022-06-19 16:19:52 +0300

    bases for xml

---
+++ favourites_movies_user_xml.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>jsonfav</title>
+   
+</head>
+<body>
+            <pre id="fav__output">
+
+            </pre>  
+            <script src="js/favourites_movies_user_xml.js" defer></script>
+</body> 
+</html>
+
