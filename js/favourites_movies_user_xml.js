3ee7ebe249bc0712c32211adfaa9bde0a90b0d07
MarcuEduardA3 <marcu.eduard37@gmail.com>
2022-06-19 16:36:12 +0300

    Update favourites_movies_user_xml.js

--- js/favourites_movies_user_xml.js
+++ js/favourites_movies_user_xml.js
@@ -19,7 +19,7 @@ const res = await fetch(`https://vira3.herokuapp.com/api/favourite/read_user_fav
     let output = OBJtoXML(reply);
     console.log(output);
     // var str= JSON.stringify(reply);
-    // document.body.appendChild(document.createElement("pre")).innerHTML=str;
+     document.body.appendChild(document.createElement("pre")).innerHTML=output;
 }
 getFavJson();
 function OBJtoXML(obj) {
