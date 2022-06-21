8868302d2c6a901ab06591d04407ef3b8d367c2d
liviuq <petrache.andrei1@gmail.com>
2022-06-21 21:16:36 +0300

    Deleted comments

--- test_d3.php
+++ test_d3.php
@@ -7,19 +7,6 @@
     <link rel="stylesheet" href="sass/d3svg.css">
 </head>
 <body>
-            <!-- <div id="d3-container">
-
-            </div>   -->
-            <!-- <script src="js/d3svg.js" defer></script>   -->
-                        
-            <!-- <svg class="chart" width="4200" height="150">
-                <g class="bar">
-                    <text x="0" y="30" dy=".35em">nota 10</text>
-                    <rect x ="55" width="40" height="19" y="20" ></rect>
-                </g>
-            </svg> -->
-
-            
             <script src="js/d3svg.js" defer></script>
 </body> 
 </html>
