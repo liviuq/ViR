eeb744994ffe2c7e218c8aa917ff0625c765a894
MarcuEduardA3 <marcu.eduard37@gmail.com>
2022-06-21 14:37:28 +0300

    Help page

---
+++ help.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="sass/aboutusViR.css">
+    <title>Help</title>
+</head>
+<body>
+<div class="about">
+    <div class="about__items">
+        <div class="item__image">
+            <img src="https://picsum.photos/500/500" alt="">
+        </div>
+        <div class="item__text">
+            <h1>Help Page</h1>
+            <div class="text__intro">
+                There are different buttons on this website, each with a clear purpose.
+                <br>
+                CSV&SVG will show you different informations about the movies on this website.
+                <br>
+                RSS is a really simple syndication, a newsletter if you will.
+                <br>
+                On a movie page there will be a few options: adding/removin the movie to/from favourites, leaving a review, getting a JSON/XML representation of the movies, but for all of these you will need to be logged in.
+            </div>
+            <a href="index.php">Go back to the movies</a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
